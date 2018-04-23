73019ad472a1da38e9ba54fc0306513894798288
Alan Leal <alanmeira@gmail.com>
2018-04-23 20:20:23 -0300

    fixes

--- test/paratydigital/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns paratydigital.core-test
-  (:require [clojure.test :refer :all]
-            [paratydigital.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
